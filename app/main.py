09258bfcd53fd052072284375db5e61471e51fa6
Airborne101st <akshatsharma101st@gmail.com>
2025-06-15 14:41:14 +0530

    Added main.py

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.routes import auth
+from sqlmodel import SQLModel
+from app.db.session import engine
+from app.models.user import User
+from app.models.fund import Fund
+from app.models.portfolio import Portfolio
+
+app = FastAPI()
+
+SQLModel.metadata.create_all(engine)
+
+app.include_router(auth.router)
+
+
+
