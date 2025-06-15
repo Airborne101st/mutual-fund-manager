647d6d1ba689f6fa8fe66ac2d3a193c38cd51430
Airborne101st <akshatsharma101st@gmail.com>
2025-06-15 21:15:52 +0530

    Added loading env variables in session

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,10 @@
 from sqlmodel import SQLModel, create_engine, Session
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:takeoff@localhost/mutualfund")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
