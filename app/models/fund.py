2bbfccebe02d80cf3850cdba8415f51c02a083b2
Airborne101st <akshatsharma101st@gmail.com>
2025-06-15 17:19:46 +0530

    Added model for fund

---
+++ app/models/fund.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class Fund(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    fund_code: str = Field(unique=True)
+    fund_name: str
+    fund_family: str
+    latest_nav: str
