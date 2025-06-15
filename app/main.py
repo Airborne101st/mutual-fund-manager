720d9c0b0f395678ee1ca7eb9fc381a03c9846d2
Airborne101st <akshatsharma101st@gmail.com>
2025-06-15 21:15:14 +0530

    Added routes in main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import auth
+from app.routes import funds
+from app.routes import portfolio
 from sqlmodel import SQLModel
 from app.db.session import engine
 from app.models.user import User
@@ -11,6 +13,6 @@ app = FastAPI()
 SQLModel.metadata.create_all(engine)
 
 app.include_router(auth.router)
-
-
+app.include_router(funds.router)
+app.include_router(portfolio.router)
 
