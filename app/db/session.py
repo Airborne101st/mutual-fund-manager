f5effe4baacc6b54529b435bfa75d50afe423d68
Airborne101st <akshatsharma101st@gmail.com>
2025-06-19 22:49:27 +0530

    Added creation of async session factory in session.py, added async_engine and converted get)session() into async

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,21 @@
-from sqlmodel import SQLModel, create_engine, Session
-import os
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from typing import AsyncGenerator
+import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, echo=True)
+async_engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
-def get_session():
-    with Session(engine) as session:
+async_session = sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
         yield session
