4f3d98e63947b75f539cd9894d18e15366a37f32
Airborne101st <akshatsharma101st@gmail.com>
2025-06-20 16:46:24 +0530

    Added field amount in portfolio schema

--- app/schemas/portfolio.py
+++ app/schemas/portfolio.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 
 class FundPurchase(BaseModel):
     fund_code: str = Field(..., example="102345")
-    units: int = Field(..., gt=0, example=10)
+    amount: float = Field(..., gt=0, example=1000.0)
     user_id: int = Field(..., gt=0, example=1)
